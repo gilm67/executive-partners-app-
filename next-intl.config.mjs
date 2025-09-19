75348424c48acf94e70503015f4e102e27e22766
Gil Malalel <your-github-email@example.com>
2025-09-19 08:27:31 +0200

    i18n: keep default locale prefix (avoid / <-> /en loop)

--- next-intl.config.mjs
+++ next-intl.config.mjs
@@ -2,5 +2,5 @@ export default {
   locales: ['en', 'fr', 'de'],
   defaultLocale: 'en',
   // Use 'as-needed' if "/" should be English and only non-default locales are prefixed
-  localePrefix: 'as-needed'
+  localePrefix: "always"
 };
