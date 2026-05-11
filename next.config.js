ba281b267289b9d4ff290a0155735359751b5bf0
Gilchalem <gil.malalel@gmail.com>
2026-05-11 16:20:16 +0200

    perf: fix image config - remove invalid qualities key

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const nextConfig = {
     minimumCacheTTL: 31536000,
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    qualities: [75],
   },
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: false },
